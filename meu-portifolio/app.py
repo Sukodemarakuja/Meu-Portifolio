441b37e89a399064dfcaff66d9a59ddc06110f19
Joao Suko Henrique <joojwallker@gmail.com>
2026-06-15 23:15:24 -0300

    Preparando para o deploy adicionando o requirements.txt e o runtime.txt

--- meu-portifolio/app.py
+++ meu-portifolio/app.py
@@ -1,28 +1,18 @@
+import json
 from flask import Flask, render_template
 
 app= Flask(__name__)
 
 @app.route('/')
 def home():
-    # Uma lista de projetos reais (dados simulando um banco de dados)
-    meus_projetos = [
-        {
-            "titulo": "Este meu Portfólio",
-            "descricao": "Criado usando Flask para servir páginas dinâmicas, com estilos modernos em CSS.",
-            "link": "#"
-        },
-        {
-            "titulo": "Scripts de Automação",
-            "descricao": "Conjunto de scripts em Python desenvolvidos para automatizar tarefas cotidianas.",
-            "link": "https://github.com/sukodemarakuja"
-        }
-    ]
-    
-    return render_template('index.html', projetos=meus_projetos)
 
-@app.route('/contato')
-def contato():
-    return "<h1> Caso queira entrar em contato comigo, envie um email para: <p> <strong>jaohenrick04@gmail.com</strong> </p> </h1>"
+    try:
+        with open('projetos.json', 'r', encoding='utf-8') as card_projetos:
+            projetos_brutos = json.load(card_projetos)
+        return render_template('index.html', projetos_brutos=projetos_brutos)
+    except FileNotFoundError:
+        print('Erro no banco de dados! Tentando novamente...')
+        projetos_brutos=[]
 
 if __name__ == '__main__':
     app.run(debug=True)
