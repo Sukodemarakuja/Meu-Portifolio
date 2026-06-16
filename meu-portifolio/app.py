8ece53ff074ab0bdd9c79e44ab6eb798c1f4f6e7
Joao Suko Henrique <joojwallker@gmail.com>
2026-06-16 10:34:05 -0300

    Otimizando o portfólio tirando bibliotecas desnecessárias

--- meu-portifolio/app.py
+++ meu-portifolio/app.py
@@ -6,13 +6,17 @@ app= Flask(__name__)
 @app.route('/')
 def home():
 
+    projetos_brutos = []
+
     try:
         with open('projetos.json', 'r', encoding='utf-8') as card_projetos:
             projetos_brutos = json.load(card_projetos)
-        return render_template('index.html', projetos_brutos=projetos_brutos)
-    except FileNotFoundError:
+    except Exception as e:
+        projetos_brutos = []
         print('Erro no banco de dados! Tentando novamente...')
-        projetos_brutos=[]
+
+    return render_template('index.html', projetos_brutos=projetos_brutos)
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
